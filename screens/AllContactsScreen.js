7e0bb24b732abcbb2a8dc76baa15483fce6ea023
Ivan Sadovyi <ivan.sadovyi@inoxoft.com>
2022-01-19 14:40:07 +0200

    Create AllContacts and ContactDetails screens

---
+++ screens/AllContactsScreen.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+const AllContactsScreen = (props) => {
+  return (
+    <View>
+      <Text>All Contacts Screen</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default AllContactsScreen;
---
+++ screens/ContactDetailsScreen.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+const ContactDetailsScreen = (props) => {
+  return (
+    <View>
+      <Text>Contact Details Screen</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default ContactDetailsScreen;
